dd4314f8411f9fad650ad1e427a0ab1055b7ec52
Pratik Jagadale <70603606+Pratik-Jagadale@users.noreply.github.com>
2021-01-01 13:24:04 +0530

    Add files via upload

---
+++ Data Set/DS_Product_Purchase.vb
@@ -0,0 +1,8 @@
+﻿Partial Class DS_Product_Purchase
+End Class
+
+Namespace DS_Product_PurchaseTableAdapters
+    
+    Partial Public Class stbl_Product_Purchase_DetailsTableAdapter
+    End Class
+End Namespace
---
+++ Data Set/DS_Student_Purchase_Details.vb
@@ -0,0 +1,8 @@
+﻿Partial Class DS_Student_Purchase_Details
+End Class
+
+Namespace DS_Student_Purchase_DetailsTableAdapters
+    
+    Partial Public Class stbl_Student_Purchase_DetailsTableAdapter
+    End Class
+End Namespace
